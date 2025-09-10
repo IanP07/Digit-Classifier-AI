df0cae7ce822dfd601a205add0df25c435c26d61
Ian Panait <150859108+IanP07@users.noreply.github.com>
2025-09-10 18:26:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -84,5 +84,6 @@ async def keep_alive():
 
 # Run the server
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os, uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
